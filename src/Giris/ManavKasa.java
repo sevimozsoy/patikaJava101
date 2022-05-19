5e3761405fc2c58663f460a05f50080d8313817a
sevimozsoy <sevimozsoy30@gmail.com>
2022-05-19 22:48:40 +0300

    Manav Kasa uygulaması eklendi.

---
+++ src/Giris/ManavKasa.java
@@ -0,0 +1,34 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class ManavKasa {
+    public static void main(String[] args){
+        double toplam = 0, armut = 2.14, elma = 3.67, domates = 1.11, muz= 0.95, patlican = 5.00;
+        int kilo;
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Armut Kaç Kilo ?:");
+        kilo = scn.nextInt();
+        toplam += (kilo * armut);
+
+        System.out.print("Elma  Kaç Kilo ?:");
+        kilo = scn.nextInt();
+        toplam += (kilo * elma);
+
+        System.out.print("Domates  Kaç Kilo ?:");
+        kilo = scn.nextInt();
+        toplam += (kilo * domates);
+
+        System.out.print("Muz  Kaç Kilo ?:");
+        kilo = scn.nextInt();
+        toplam += (kilo * muz);
+
+        System.out.print("Patlıcan  Kaç Kilo ?:");
+        kilo = scn.nextInt();
+        toplam += (kilo * patlican);
+
+        System.out.println("Toplam tutar: " + toplam + "TL");
+    }
+}
